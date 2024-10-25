081a36d379cd39a712aa6ad2c89703d6ead03943
Kelvin Wallace <kw222sw@student.lnu.se>
2024-10-25 13:28:05 +0200

    Previously I had cloned the module repo and used it but I was unable to commit it. I have now just copied over the files so that I can commit, push and publish the website!

---
+++ src/assignmentL2/workoutModule/exerciseGenerator.js
@@ -0,0 +1,41 @@
+import { getExerciseData } from '../API/exerciseAPI.js'
+
+
+/**
+ * Generates exercises based on the specified muscle, type, and difficulty.
+ *
+ * @async
+ * @returns {Promise<Array>} - A promise that resolves to an array of exercises.
+ */
+class ExerciseGenerator {
+  
+  /**
+   * Generates exercises based on the specified muscle, type, and difficulty.
+   *
+   * @async
+   * @param {string} muscle - The muscle for which exercises need to be generated.
+   * @param {string} [type=''] - The type of exercises to be generated. (Optional)
+   * @param {string} [difficulty=''] - The difficulty level of exercises to be generated. (Optional)
+   * @returns {Promise<Array>} - A promise that resolves to an array of exercises.
+   */
+  async generateExercises (muscle, type = '', difficulty = '') {
+    try {
+      return await getExerciseData(muscle, type, difficulty)
+    } catch (error) {
+      this.logError('generateExercises', error)
+      throw new Error('Failed to generate exercises')
+    }
+  }
+
+  /**
+   * Logs errors for debugging purposes.
+   *
+   * @param {string} functionName - The name of the function where the error occurred.
+   * @param {Error} error - The error object.
+   */
+  logError (functionName, error) {
+    console.error(`Error in ${functionName}:`, error)
+  }
+}
+
+export default ExerciseGenerator
